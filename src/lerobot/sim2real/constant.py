b2c8ea6a67bd384739c3c8d76163108b6b011087
Jialei Li <lijialei829@gmail.com>
2026-06-16 22:25:37 +0200

    add camera id by path

--- src/lerobot/sim2real/constant.py
+++ src/lerobot/sim2real/constant.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from pathlib import Path
 
 ###### Paths ######
@@ -5,7 +6,25 @@ from pathlib import Path
 LOG_FOLDER = Path(__file__).resolve().parent.parent.parent.parent / "isaacsim_sim2real/logs"
 
 
-###### Constants ######
+###### Cameras ######
+
+DEV_V4L_BY_PATH = "/dev/v4l/by-path"
+LAPTOP_LEFT_USB_1 = f"{DEV_V4L_BY_PATH}/pci-0000:00:14.0-usb-0:1:1.0-video-index0"
+LAPTOP_RIGHT_USB_1 = f"{DEV_V4L_BY_PATH}/pci-0000:00:14.0-usb-0:2:1.0-video-index0"
+# usb hub: connect to laptop right usb 1
+# NOTE: socket ID 1-4 starting from the indicator light on the usb hub
+LAPTOP_RIGHT_USB_1_USB_HUB_SOCKET_1 = f"{DEV_V4L_BY_PATH}/pci-0000:00:14.0-usb-0:2.1:1.0-video-index0"
+LAPTOP_RIGHT_USB_1_USB_HUB_SOCKET_2 = f"{DEV_V4L_BY_PATH}/pci-0000:00:14.0-usb-0:2.2:1.0-video-index0"
+LAPTOP_RIGHT_USB_1_USB_HUB_SOCKET_3 = f"{DEV_V4L_BY_PATH}/pci-0000:00:14.0-usb-0:2.3:1.0-video-index0"
+LAPTOP_RIGHT_USB_1_USB_HUB_SOCKET_4 = f"{DEV_V4L_BY_PATH}/pci-0000:00:14.0-usb-0:2.4:1.0-video-index0"
+
+class So101Camera(str, Enum):
+    """The usb path of camera on usb hub for SO101 robot arm system."""
+    wrist_cam = LAPTOP_RIGHT_USB_1_USB_HUB_SOCKET_3
+    side_cam = LAPTOP_RIGHT_USB_1_USB_HUB_SOCKET_4
+
+
+###### Calibration Names ######
 
 SO101_NEW_CALIB = "so101_new_calib"
 SO101_OLD_CALIB = "so101_old_calib"
